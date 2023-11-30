9094fe308e861179fe58f2da1864813acab0dbda
Pranav Raj S <pranav@chatwoot.com>
2023-11-30 11:33:53 -0800

    feat: Add an API for contributors

---
+++ server/pages/api/contributors.js
@@ -0,0 +1,9 @@
+import db from '../../data/db.json'
+
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).json(db.authors)
+  } else {
+    res.status(404)
+  }
+}
